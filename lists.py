bd753620befe3a6b716d48908a6187351133f8d9
Julio Relosa <Julio@Madalines-MacBook-Air.local>
2024-10-07 17:58:02 -0700

    Finisted Excercise 1

---
+++ lists.py
@@ -0,0 +1,14 @@
+movies = ['Gladiator', 'Finding Dory', 'Deadpool and Wolverine']
+ratings = [8.5, 7, 8.0]
+genres = ['Action', 'family', 'action']
+
+print( movies[1] )
+print(' ')
+
+ratings[0] = 9
+
+movies.append('The Dark Knight')
+ratings.append(9.2)
+genres.append('action')
+
+print(ratings[3])
